9e7de9942bd36e4b01e1a989132d3590a87ca29b
JoelHe <10458040@qq.com>
2019-05-19 18:00:39 +0800

    worker系统

--- xsl_worker_service/src/main/java/service/impl/GenerateAccountImpl.java
+++ xsl_worker_service/src/main/java/service/impl/GenerateAccountImpl.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pojo.XslAccount;
-import quartz.CalcelTaskJob;
 import resource.TaskInfoResource;
 import service.GenerateAccount;
 import util.GsonSingle;
